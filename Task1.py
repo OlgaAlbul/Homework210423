3798d2dc3ce203f0f1d13b1443b99b62003571da
AlbulOlya <albul.olya@gmail.com>
2023-04-24 12:51:52 +0500

    Task 1 part 2 solved

--- Task1.py
+++ Task1.py
@@ -5,5 +5,14 @@ def factorial (n):
     if n==1:
         return 1
     return n*factorial(n-1)
+
+
+def triangular_number (n):
+    if n==0:
+        return 0
+    return n+triangular_number(n-1)
 N = int(input('Введите число: '))
 print(factorial(N))
+print(triangular_number(N))
+
+
